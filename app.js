8bccd7a3b90e5fc47b70fb92f61460759c6b9d9b
Leslie Kornes <lesliekornes@gmail.com>
2019-01-20 10:37:08 -0500

    Add log with conditional styles.

--- app.js
+++ app.js
@@ -3,24 +3,35 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100, 
-        gameIsRunning: false
+        gameIsRunning: false, 
+        turns: []
     }, 
     methods: {
         startGame() {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack() {
-            this.monsterHealth -= this.calculateDamage(3, 10);
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage + ' damage!'
+            });
             if (this.checkWin()) {
                 return;
             }
-            
             this.monsterAttacks();
         },
         specialAttack() {
-            this.monsterHealth -= this.calculateDamage(10, 20);
+            let damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage + ' damage w Special Attack!'
+            });
             if (this.checkWin()) {
                 return;
             }
@@ -32,13 +43,22 @@ new Vue({
             } else {
                 this.playerHealth = 100;
             }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals 10 damage.'
+            });
             this.monsterAttacks();
         },
         giveUp() {
-
+            this.gameIsRunning = false;
         },
         monsterAttacks() {
-            this.playerHealth -= this.calculateDamage(5, 12);
+            let damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for ' + damage + ' damage!'
+            })
             this.checkWin();
         },
         calculateDamage(min, max) {
